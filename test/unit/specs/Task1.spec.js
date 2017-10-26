41a075d1fb5592902115b34c904c104d5ae84508
Sergey Klevakin <justerest@yandex.com>
2017-10-26 22:13:06 +0500

    Update README.md

--- test/unit/specs/Task1.spec.js
+++ test/unit/specs/Task1.spec.js
@@ -1,8 +1,7 @@
 import Task1 from '@/tasks/Task1';
 
 describe('Task1', () => {
-  it('should render correct contents', done => {
+  it('should render correct contents', () => {
     expect(Task1().console.replace(/(<br>|\s)/g, '')).to.equal('10515');
-    done();
   });
 });
